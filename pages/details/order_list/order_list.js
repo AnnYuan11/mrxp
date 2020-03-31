9d8cf9e0069e5fe60d629233a2c98e9b76548af9
anyuan <anyuan@qq.com>
2020-03-31 08:54:45 +0800

    20200331

--- pages/details/order_list/order_list.js
+++ pages/details/order_list/order_list.js
@@ -5,14 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    winWidth: 0,
+    winHeight: 0,
+    currentTab: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that=this;
+    /**
+     * 获取当前设备的宽高
+     */
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });
   },
 
   /**
@@ -62,5 +75,22 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  swichNav: function (e) {
+    var that = this;
+    console.log(e)
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  },
+
+  bindChange: function (e) {
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+
+  },
 })
